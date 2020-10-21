8e1450d4dcf125c1ad623f676ed224f0cdd0d804
abutler <abutler@psl.nmsu.edu>
2020-10-21 15:06:27 -0600

    Added helper classes for auto-updating an app

---
+++ src/main/java/org/observe/ext/util/GitHubApiHelper.java
@@ -0,0 +1,461 @@
+package org.observe.ext.util;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JProgressBar;
+
+import org.observe.ObservableValue;
+import org.observe.SettableValue;
+import org.observe.util.swing.JustifiedBoxLayout;
+import org.observe.util.swing.WindowPopulation;
+import org.qommons.QommonsUtils;
+import org.qommons.io.BetterFile;
+import org.qommons.io.FileUtils;
+import org.qommons.io.Format;
+import org.qommons.json.JsonSerialReader;
+import org.qommons.json.JsonSerialReader.JsonParseType;
+import org.qommons.json.JsonSerialReader.StructState;
+import org.qommons.json.SAJParser;
+import org.qommons.threading.QommonsTimer;
+
+public class GitHubApiHelper {
+	public static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = ThreadLocal.withInitial(() -> {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		format.setTimeZone(TimeZone.getTimeZone("GMT"));
+		return format;
+	});
+
+	private String theGitHubUrl = "https://api.github.com/";
+	private String theRepositoryOwner;
+	private String theRepository;
+
+	public GitHubApiHelper(String repoOwner, String repoName) {
+		theRepositoryOwner = repoOwner;
+		theRepository = repoName;
+	}
+
+	public List<Release> getReleases() throws IOException {
+		String url = theGitHubUrl;
+		url = append(url, "repos/") + theRepositoryOwner + "/" + theRepository + "/releases";
+		HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+		connection.setRequestProperty("User-Agent", theRepositoryOwner);
+		connection.connect();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+			List<Release> releases = new ArrayList<>();
+			JsonSerialReader json = new JsonSerialReader(reader);
+			json.startArray();
+			List<Asset> assets = new ArrayList<>();
+			while (json.getNextItem(true, false).getType() == JsonParseType.OBJECT) {
+				assets.clear();
+				String name = null, tagName = null, htmlUrl = null, desc = null;
+				Instant date = null;
+				boolean preRelease = false, draft = false;
+				String property = json.getNextProperty();
+				while (property != null) {
+					switch (property) {
+					case "html_url":
+						htmlUrl = json.parseString();
+						break;
+					case "name":
+						name = json.parseString();
+						break;
+					case "tag_name":
+						tagName = json.parseString();
+						break;
+					case "body":
+						desc = json.parseString();
+						break;
+					case "draft":
+						draft = json.parseBoolean();
+						break;
+					case "prerelease":
+						preRelease = json.parseBoolean();
+						break;
+					case "published_at":
+						date = DATE_FORMAT.get().parse(json.parseString()).toInstant();
+						break;
+					case "assets":
+						parseAssets(json, assets);
+						break;
+					}
+
+					property = json.getNextProperty();
+				}
+				releases.add(new Release(name, tagName, htmlUrl, desc, date, draft, preRelease, QommonsUtils.unmodifiableCopy(assets)));
+			}
+			return releases;
+		} catch (SAJParser.ParseException | java.text.ParseException e) {
+			throw new IOException("Failed to parse repository list", e);
+		}
+	}
+
+	public Release getLatestRelease(Class<?> clazz) throws IOException {
+		String currentVersion = getCurrentRelease(clazz);
+		boolean preRelease = currentVersion != null && currentVersion.startsWith("0.");
+		List<Release> releases = getReleases();
+		for (Release r : releases) {
+			if (r.isDraft) {
+				continue;
+			} else if (!preRelease && r.isPreRelease) {
+				continue;
+			}
+			if (!VERSION_PATTERN.matcher(r.getTagName()).matches()) {
+				continue;
+			}
+			if (r.getAssets().isEmpty()) {
+				continue;
+			}
+			return r;
+		}
+		return null;
+	}
+
+	private static String append(String url, String suffix) {
+		if (url.charAt(url.length() - 1) != '/') {
+			url += "/";
+		}
+		return url + suffix;
+	}
+
+	private static void parseAssets(JsonSerialReader json, List<Asset> assets) throws IOException, SAJParser.ParseException {
+		StructState state = json.startArray();
+		while (json.getNextItem(true, false).getType() == JsonParseType.OBJECT) {
+			String name = null, apiUrl = null, desc = null;
+			String property = json.getNextProperty();
+			while (property != null) {
+				switch (property) {
+				case "name":
+					name = json.parseString();
+					break;
+				case "browser_download_url":
+					apiUrl = json.parseString();
+					break;
+				case "label":
+					desc = json.parseString();
+					break;
+				}
+
+				property = json.getNextProperty();
+			}
+			assets.add(new Asset(name, apiUrl, desc));
+		}
+		json.endArray(state);
+	}
+
+	public static String getCurrentRelease(Class<?> clazz) {
+		Package pkg = clazz.getPackage();
+		String currentVersion = null;
+		while (pkg != null && (currentVersion = pkg.getImplementationVersion()) == null) {
+			String pkgName = pkg.getName();
+			int lastDot = pkgName.lastIndexOf('.');
+			if (lastDot < 0) {
+				break;
+			}
+			pkg = Package.getPackage(pkgName.substring(0, lastDot));
+		}
+		return currentVersion;
+	}
+
+	public static final Pattern VERSION_PATTERN = Pattern.compile("v?([0-9]+)\\.([0-9]+)\\.([0-9]+)");
+
+	public boolean checkForNewVersion(Class<?> clazz, String title, Image image, Function<Release, Boolean> check,
+			Consumer<Release> upgradeRejected, Runnable afterCheck) throws IllegalStateException, IOException {
+		return _upgrade(clazz, title, image, check, upgradeRejected, afterCheck, true);
+	}
+
+	public void upgradeToLatest(Class<?> clazz, String title, Image image) throws IllegalStateException, IOException {
+		_upgrade(clazz, title, image, null, null, null, false);
+	}
+
+	boolean _upgrade(Class<?> clazz, String title, Image image, Function<Release, Boolean> check, Consumer<Release> upgradeRejected,
+			Runnable afterCheck, boolean askUser) throws IllegalStateException, IOException {
+		BetterFile jarFile = FileUtils.getClassFile(clazz);
+		while (jarFile != null && !jarFile.getName().toLowerCase().endsWith(".jar")) {
+			jarFile = jarFile.getParent();
+		}
+		if (jarFile == null) {
+			return false;
+		}
+		File javaJarFile = new File(jarFile.getPath());
+		JFrame[] frame = new JFrame[1];
+		try {
+			String currentVersion = getCurrentRelease(clazz);
+			if (currentVersion == null) {
+				return false;
+			}
+			Matcher cvMatcher = VERSION_PATTERN.matcher(currentVersion);
+			if (!cvMatcher.matches()) {
+				return false;
+			}
+			List<Release> releases = getReleases();
+			Release latest = null;
+			Asset asset = null;
+			Matcher rv = null;
+			for (Release r : releases) {
+				if (r.isDraft) {
+					continue;
+				} else if (!currentVersion.startsWith("0.") && r.isPreRelease) {
+					continue;
+				}
+				rv = VERSION_PATTERN.matcher(r.getTagName());
+				if (!rv.matches()) {
+					continue;
+				}
+				for (Asset a : r.getAssets()) {
+					if (a.getName().endsWith(".jar")) {
+						asset = a;
+					}
+				}
+				if (asset == null) {
+					continue;
+				}
+				latest = r;
+				break;
+			}
+			if (latest == null) {
+				return true;
+			}
+			int comp = Integer.compare(Integer.parseInt(cvMatcher.group(1)), Integer.parseInt(rv.group(1)));
+			if (comp == 0) {
+				comp = Integer.compare(Integer.parseInt(cvMatcher.group(2)), Integer.parseInt(rv.group(2)));
+			}
+			if (comp == 0) {
+				comp = Integer.compare(Integer.parseInt(cvMatcher.group(3)), Integer.parseInt(rv.group(3)));
+			}
+			if (comp >= 0) {
+				return true;
+			}
+			if (check != null && !check.apply(latest)) {
+				return true;
+			}
+			SettableValue<Boolean> hasChosen = SettableValue.build(boolean.class).withValue(!askUser).safe(false).build();
+			Release release = latest;
+			String assetUrl = asset.getApiUrl();
+			SettableValue<Boolean> canceled = SettableValue.build(boolean.class).withValue(false).build();
+			JProgressBar progress = new JProgressBar();
+			frame[0] = WindowPopulation.populateWindow(new JFrame(), null, true, false)//
+					.withTitle(title).withIcon(image)//
+					.withVContent(content -> {
+						content.addLabel(null, ObservableValue.of("A new version of " + title + " is available: " + release.getTagName()),
+								Format.TEXT, null);
+						content.addLabel(null, hasChosen.map(c -> c ? "Upgrading..." : "Would you like to upgrade?"), Format.TEXT, null);
+						progress.setStringPainted(true);
+						progress.setIndeterminate(true);
+						progress.setString("Downloading new release");
+						progress.setVisible(false);
+						content.addHPanel(null, new JustifiedBoxLayout(false).mainCenter(), buttons -> {
+							buttons.visibleWhen(hasChosen.map(c -> !c));
+							buttons.addButton("Yes", __ -> {
+								hasChosen.set(true, null);
+							}, null);
+							buttons.addButton("No", __ -> {
+								if (upgradeRejected != null) {
+									upgradeRejected.accept(release);
+								}
+								frame[0].setVisible(false);
+							}, null);
+						});
+						content.addComponent(null, progress, p -> p.fill());
+						content.addHPanel(null, new JustifiedBoxLayout(false).mainCenter(), buttons -> buttons.addButton("Cancel", __ -> {
+							canceled.set(true, null);
+						}, b -> b.visibleWhen(hasChosen).disableWith(canceled.map(c -> c ? "Canceling..." : null))));
+					}).run(null).getWindow();
+			hasChosen.changes().act(evt -> {
+				if (!evt.getNewValue()) {
+					return;
+				}
+				progress.setVisible(true);
+				QommonsTimer.getCommonInstance().offload(() -> {
+					try {
+						doUpgrade(assetUrl, javaJarFile, progress, canceled);
+						EventQueue.invokeLater(() -> {
+							frame[0].setVisible(false); // User must've canceled
+						});
+					} catch (IOException e) {
+						e.printStackTrace();
+						EventQueue.invokeLater(() -> {
+							JOptionPane.showMessageDialog(frame[0], title + " could not be updated: " + e.getMessage(), "Update Failed",
+									JOptionPane.ERROR_MESSAGE);
+							frame[0].setVisible(false);
+						});
+					}
+				});
+			});
+			frame[0].addComponentListener(new ComponentAdapter() {
+				@Override
+				public void componentHidden(ComponentEvent e) {
+					canceled.set(true, null);
+					if (afterCheck != null) {
+						afterCheck.run();
+					}
+				}
+			});
+			return true;
+		} finally {
+			if (frame[0] == null || !frame[0].isVisible()) {
+				QuarkVersionUpdater.deleteUpdater(javaJarFile);
+				if (afterCheck != null) {
+					afterCheck.run();
+				}
+			}
+		}
+	}
+
+	private void doUpgrade(String assetUrl, File jarFile, JProgressBar progress, ObservableValue<Boolean> canceled) throws IOException {
+		File newVersion = new File(jarFile.getAbsoluteFile().getParentFile(),
+				jarFile.getName().substring(0, jarFile.getName().length() - 4) + ".new.jar");
+		HttpsURLConnection connection = (HttpsURLConnection) new URL(assetUrl).openConnection();
+		boolean redirect;
+		do {
+			redirect = false;
+			switch (connection.getResponseCode()) {
+			case 301:
+			case 302:
+			case 307:
+				connection = (HttpsURLConnection) new URL(connection.getHeaderField("Location")).openConnection();
+				redirect = true;
+				break;
+			}
+		} while (redirect);
+		int length = connection.getContentLength();
+		EventQueue.invokeLater(() -> {
+			progress.setMaximum(length);
+			progress.setIndeterminate(false);
+		});
+		int downloaded = 0;
+		try (InputStream in = new BufferedInputStream(connection.getInputStream());
+				OutputStream out = new BufferedOutputStream(new FileOutputStream(newVersion))) {
+			byte[] buffer = new byte[64 * 1024];
+			int read = in.read(buffer);
+			while (read >= 0) {
+				out.write(buffer, 0, read);
+				downloaded += read;
+				int d = downloaded;
+				EventQueue.invokeLater(() -> progress.setValue(d));
+				if (canceled.get()) {
+					return;
+				}
+				read = in.read(buffer);
+			}
+		}
+		EventQueue.invokeLater(() -> {
+			progress.setString("Preparing to install new release");
+			progress.setIndeterminate(true);
+		});
+		QuarkVersionUpdater.update(jarFile, newVersion);
+	}
+
+	public static class Release {
+		private final String theName;
+		private final String theTagName;
+		private final String theHtmlUrl;
+		private final String theDescription;
+		private final Instant thePublishedDate;
+		private final boolean isDraft;
+		private final boolean isPreRelease;
+		private final List<Asset> theAssets;
+
+		Release(String name, String tagName, String htmlUrl, String description, Instant publishedDate, boolean draft, boolean preRelease,
+				List<Asset> assets) {
+			theName = name;
+			theTagName = tagName;
+			theHtmlUrl = htmlUrl;
+			theDescription = description;
+			thePublishedDate = publishedDate;
+			isDraft = draft;
+			isPreRelease = preRelease;
+			theAssets = assets;
+		}
+
+		public String getName() {
+			return theName;
+		}
+
+		public String getTagName() {
+			return theTagName;
+		}
+
+		public String getHtmlUrl() {
+			return theHtmlUrl;
+		}
+
+		public String getDescription() {
+			return theDescription;
+		}
+
+		public Instant getPublishedDate() {
+			return thePublishedDate;
+		}
+
+		public boolean isDraft() {
+			return isDraft;
+		}
+
+		public boolean isPreRelease() {
+			return isPreRelease;
+		}
+
+		public List<Asset> getAssets() {
+			return theAssets;
+		}
+
+		@Override
+		public String toString() {
+			return theName;
+		}
+	}
+
+	public static class Asset {
+		private final String theName;
+		private final String theApiUrl;
+		private final String theDescription;
+
+		Asset(String name, String apiUrl, String description) {
+			theName = name;
+			theApiUrl = apiUrl;
+			theDescription = description;
+		}
+
+		public String getName() {
+			return theName;
+		}
+
+		public String getApiUrl() {
+			return theApiUrl;
+		}
+
+		public String getDescription() {
+			return theDescription;
+		}
+
+		@Override
+		public String toString() {
+			return theName;
+		}
+	}
+}
---
+++ src/main/java/org/observe/ext/util/QuarkVersionUpdater.java
@@ -0,0 +1,133 @@
+package org.observe.ext.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.ProcessBuilder.Redirect;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.swing.JOptionPane;
+
+public class QuarkVersionUpdater {
+	private static final String UPDATER_MANIFEST = "Manifest-Version: 1.0"//
+			+ "\nMain-Class: " + QuarkVersionUpdater.class.getName()//
+			+ "\nClass-Path: ." //
+			+ "\n";
+
+	public static void main(String... args) {
+		long [] lastDelay=new long[]{System.currentTimeMillis()};
+		Thread delay=new Thread(()->{
+			try{
+				int read=System.in.read();
+				while(read>=0){
+					lastDelay[0]=System.currentTimeMillis();
+					read=System.in.read();
+				}
+			} catch(IOException e){
+			}
+		}, QuarkVersionUpdater.class.getSimpleName()+" Delay");
+		delay.setDaemon(true);
+		delay.start();
+		
+		while(System.currentTimeMillis()-lastDelay[0]<250){}
+		
+		File oldVersion = new File(args[0]);
+		File newVersion = new File(args[1]);
+		try {
+			copy(new FileInputStream(newVersion), oldVersion);
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Version copy failed!", "Could not update " + args[0], JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		newVersion.delete();
+		try {
+			new ProcessBuilder("java", "-jar", args[0]).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Could not restart " + args[0], "Could not restart app", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	public static void update(File jarFile, File updatedJarFile) throws IOException, IllegalStateException {
+		// Extract this class into a standalone class file
+		File standalone = new File(jarFile.getParentFile(), QuarkVersionUpdater.class.getSimpleName() + ".jar");
+		String classFileName = QuarkVersionUpdater.class.getName();
+		int dotIdx = classFileName.lastIndexOf('.');
+		if (dotIdx >= 0) {
+			classFileName = classFileName.substring(dotIdx + 1);
+		}
+		classFileName += ".class";
+		URL resource = QuarkVersionUpdater.class.getResource(classFileName);
+		if (resource == null) {
+			throw new IllegalStateException("Could not find class file for " + QuarkVersionUpdater.class.getName());
+		}
+		String classPath = QuarkVersionUpdater.class.getName().replaceAll("\\.", "/") + ".class";
+		try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(standalone))) {
+			zip.putNextEntry(new ZipEntry("META-INF/MANIFEST.MF"));
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(zip));
+			writer.append(UPDATER_MANIFEST);
+			writer.flush();
+			zip.putNextEntry(new ZipEntry(classPath));
+			copy(resource.openStream(), zip);
+		}
+
+		List<String> args = new ArrayList<>();
+		args.add("java");
+		// args.add("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8001");
+		args.add("-jar");
+		args.add(standalone.getPath());
+		args.add(jarFile.getPath());
+		args.add(updatedJarFile.getPath());
+		Process process = new ProcessBuilder(args).redirectOutput(Redirect.INHERIT).redirectError(Redirect.INHERIT).start();
+		Thread delay = new Thread(() -> {
+			while (true) {
+				try {
+					process.getOutputStream().write(1);
+					process.getOutputStream().flush();
+				} catch (IOException e) {
+				}
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+				}
+			}
+		}, QuarkVersionUpdater.class.getSimpleName() + " Delay");
+		delay.start();
+		System.exit(0);
+	}
+
+	public static void deleteUpdater(File jarFile) {
+		File standalone = new File(jarFile.getParentFile(), QuarkVersionUpdater.class.getSimpleName() + ".jar");
+		if (standalone.exists()) {
+			standalone.delete();
+		}
+	}
+
+	public static void copy(InputStream in, File target) throws IOException {
+		try (BufferedInputStream bufferedIn = new BufferedInputStream(in); //
+				OutputStream out = new BufferedOutputStream(new FileOutputStream(target))) {
+			copy(bufferedIn, out);
+		}
+	}
+
+	static void copy(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[64 * 1028];
+		int read = in.read(buffer);
+		while (read >= 0) {
+			out.write(buffer, 0, read);
+			read = in.read(buffer);
+		}
+	}
+}
